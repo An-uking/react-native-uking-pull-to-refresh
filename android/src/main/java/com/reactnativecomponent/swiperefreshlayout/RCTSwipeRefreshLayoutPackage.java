cc625534b91390f3303fe6f48e9bafceb5f7cd66
安治龙 <ptvile@qq.com>
2017-11-09 02:22:51 +0800

    修改android模块bug

--- android/src/main/java/com/reactnativecomponent/swiperefreshlayout/RCTSwipeRefreshLayoutPackage.java
+++ android/src/main/java/com/reactnativecomponent/swiperefreshlayout/RCTSwipeRefreshLayoutPackage.java
@@ -19,8 +19,7 @@ public class RCTSwipeRefreshLayoutPackage implements ReactPackage {
 
         return Collections.emptyList();
     }
-
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
